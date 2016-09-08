ffce907175871af3a3c378f2f79d27b1a5014c27
julie1013 <julie1380@verizon.net>
2016-09-08 12:00:11 -0400

    Set up sign-up and sign in forms.
    Set up sign-up and sign in forms for the game. Still trying to get
    curl to work.

--- scripts/user-create.sh
+++ scripts/user-create.sh
@@ -1,9 +1,21 @@
-curl --include --request "POST" "http://localhost:3000" \
-  --header "Content-Type: application/json" \
-  --data $'{
-    "game": {
-      "player_x": "Some random guy",
-      "id": "1",
-      "email": example@example.com
-    }
-  }'
+# curl --include --request "POST" "http://tic-tac-toe.wdibos.com/" \
+#   --header "Content-Type: application/json" \
+#   --data $'{
+#     "game": {
+#       "player_x": "Some random guy",
+#       "id": "1",
+#       "email": example@example.com
+#     }
+#   }'
+
+curl "http://tic-tac-toe.wdibos.com/" \
+--include \
+--request POST \
+--header "Content-Type: application/json" \
+--data $"{
+ \"credentials\": {
+   \"email\": \"email@example.com\",
+   \"password\": \"lalala\",
+   \"password_confirmation\": \"lalala\"
+ }
+}"
