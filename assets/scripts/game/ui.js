f52601d460fb78eaf5ce5adc4c49c13e3ff7c505
julie1013 <julie1380@verizon.net>
2016-09-08 16:21:48 -0400

    Resized elements
    Resized the board, squares, and (temporary) tokens

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -12,7 +12,9 @@ const drawBoard = function (array) {
 //sets up the board visually
 
 const setCell = function (cell, player) {
-  $('#' + cell).html('<img src = ../../assets/images/' + player + '.png style="width: 90px">').removeClass('unoccupied').removeClass('selected').addClass('occupied').off();
+  $('#' + cell).html
+    ('<img src = ../../assets/images/' + player + '.png style="width: 130px">')
+      .off();
   logic.currentGame.game.cells[cell] = player;
 };
 //visually updates cell with player token
