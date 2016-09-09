7d5a78740f4b68111816d65d7b6b03e11fac9699
julie1013 <julie1380@verizon.net>
2016-09-09 09:27:18 -0400

    Fixed events.js
    Updated events.js so that it has the code from when the game was working.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,7 +1,20 @@
 'use strict';
 
+const getFormFields = require('../../../lib/get-form-fields');
 const playHandlers = require('./play-handlers');
+const api = require('../game-api');
+const ui = require('./ui');
 
 playHandlers.setUpHandlers();
 
-// const api = require('./api');
+const onSignUp = function(event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.signUp(data)
+    .done(ui.onSignUpSuccess)
+    .fail(ui.onError);
+};
+
+module.exports = {
+  onSignUp,
+};
